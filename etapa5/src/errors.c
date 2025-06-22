956fc2285763119ff4e491afbd21d213c43a5157
PaoloOrguim <109828458+PaoloOrguim@users.noreply.github.com>
2025-06-22 14:54:20 -0300

    Delete etapa5 directory

--- etapa5/src/errors.c
+++
@@ -1,8 +0,0 @@
-#include "errors.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-void semantic_error(int code, const char *msg, int line_number) {
-    fprintf(stderr, "Erro semântico na linha %d: %s\n", line_number, msg);
-    exit(code);
-}
