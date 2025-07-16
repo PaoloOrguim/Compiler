ba6db92b77b48d1b1e6b58a21c23ec95dbed6e11
PaoloOrguim <109828458+PaoloOrguim@users.noreply.github.com>
2025-07-16 18:02:16 -0300

    Minor update

--- etapa6/headers/asm.h
+++ etapa6/headers/asm.h
@@ -4,7 +4,7 @@
 #include "iloc.h"
 #include "tables.h"
 
-// Função principal para gerar o código assembly.
+// Funcao pra gerar assembly
 // Recebe a lista de instruções ILOC e a tabela de símbolos do escopo global.
 void asm_generate(iloc_list_t *iloc_code, struct table *global_scope);
 
